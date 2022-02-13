2c7aeb2ea535c69b0cfb1c281b6f5b3d677f8d0c
ViniciusFebasse <vinicius_serra_@hotmail.com>
2022-02-13 10:15:35 -0300

    Initial commit

---
+++ requisicao.py
@@ -0,0 +1,28 @@
+import requests
+import shutil
+import json
+import datetime
+
+
+class Repositorio:
+    def __init__(self, usuario):
+        self.usuario = usuario
+
+    def requisita(self):
+        resposta = requests.get(f'https://api.github.com/users/{self.usuario}/repos')
+
+        if resposta.status_code == 200:
+            conteudo = resposta.json()
+
+            if type(conteudo) is not int:
+                for i in range(len(conteudo)):
+                    print(conteudo[i]['name'])
+            else:
+                print(conteudo)
+
+        else:
+            print(resposta.status_code)
+
+
+repositorios = Repositorio("ViniciusFebasse")
+repositorios.requisita()
